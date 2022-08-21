b470cadf7ef0d6c43915e9ea2725c04ee5f5de8f
Joseph C Gunn <jcg5g9@lewis4-r7425-htc5-node835.cobalt.lan>
2022-08-21 16:49:14 -0500

    vcftools shell

---
+++ filtering_processing_analysis/code/shell_scripts/vcftools.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+#SBATCH -p Lewis  # use the Lewis partition
+#SBATCH -J get_goodINDs_vcfs  # give the job a custom name
+#SBATCH -o vcf_tools_maf-%j.out  # give the job output a custom name
+#SBATCH -t 0-02:00  # two hour time limit
+
+#SBATCH -N 1  # number of nodes
+#SBATCH -n 1  # number of cores (AKA tasks)
+
+# Load VCFtools module
+
+module load rss/rss-2020
+module load vcftools/vcftools-v0.1.14
+
+# Commands with srun will run on all cores in the allocation
+
+vcftools --vcf ../../../raw_data/smb_genomics.vcf --min-meanDP 15 --recode --recode-INFO-all --out ../../data/processed_vcf/01_filter_depth
