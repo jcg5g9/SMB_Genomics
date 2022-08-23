fec9a3a9cb4bca8a674fa06a061a5a1028860bbe
Joseph C Gunn <jcg5g9@lewis4-r7425-htc5-node835.cobalt.lan>
2022-08-23 12:03:19 -0500

    update

--- filtering_processing_analysis/code/shell_scripts/vcftools.sh
+++ filtering_processing_analysis/code/shell_scripts/vcftools.sh
@@ -15,8 +15,21 @@ module load vcftools/vcftools-v0.1.14
 
 # Commands with srun will run on all cores in the allocation
 
-#vcftools --vcf ../../../raw_data/smb_genomics.vcf --minDP 15 --recode --recode-INFO-all --out ../../data/processed_vcf/01_filter_depth
-#vcftools --vcf ../../data/processed_vcf/01_filter_depth.vcf --minQ 20 --recode --recode-INFO-all --out ../../data/processed_data/02_filter_qual
-#vcftools --vcf ../../data/processed_vcf/02_filter_qual.vcf --max-missing 0.8 --recode --recode-INFO-all --out 03_filter_missing
+## generate a file of missing genotype call rates for all samples
+vcftools --vcf ../../../raw_data/smb_genomics.vcf --missing-indv --out ../../data/filtering_data/smb_genomics_missing
 
-vcftools --vcf ../../data/processed_vcf/02_filter_qual.vcf --missing-indv --out ../../data/filtering_data/smb_genomics_missing_genotype_again
+
+
+
+## filter out SNPs with less than 15X depth of coverage
+# vcftools --vcf ../../../raw_data/smb_genomics.vcf --minDP 15 --recode --recode-INFO-all --out ../../data/processed_vcf/01_filter_depth
+
+## filter out SNPs with phred quality score less than or equal to 20
+# vcftools --vcf ../../data/processed_vcf/01_filter_depth.vcf --minQ 20 --recode --recode-INFO-all --out ../../data/processed_vcf/02_filter_qual
+
+## generate a file of missing genotype call rates for all samples
+vcftools --vcf ../../data/processed_vcf/02_filter_qual.vcf --missing-indv --out ../../data/filtering_data/smb_missing_genotypes
+
+#vcftools --vcf ../../data/processed_vcf/02_filter_qual.vcf --max-missing 0.8 --recode --recode-INFO-all --out 03_filter_missing
+#vcftools --vcf ../../data/processed_vcf/.vcf --missing-indv --out ../../data/filtering_data/smb_missing_AGAIN
+
