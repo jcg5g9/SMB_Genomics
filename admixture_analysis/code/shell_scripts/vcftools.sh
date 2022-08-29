c44929f86dffaee1f4ee5870459c9554636c0fd6
Joe Gunn <jcg5g9@mail.missouri.edu>
2022-08-29 08:47:57 -0400

    filter admixed individuals vcftools

--- admixture_analysis/code/shell_scripts/vcftools.sh
+++ admixture_analysis/code/shell_scripts/vcftools.sh
@@ -16,5 +16,7 @@ module load vcftools/vcftools-v0.1.14
 # Commands with srun will run on all cores in the allocation
 
 ## filter 01: omit Spotted Bass X Neosho Bass hybrid (BFC10) 
-vcftools --vcf ../../data/processed_vcf/popgen.vcf --remove ../../data/filtering_data/bfc10.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_popgen_spb_hybrid
+#vcftools --vcf ../../data/processed_vcf/popgen.vcf --remove ../../data/filtering_data/bfc10.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_popgen_spb_hybrid
 
+## filter 02: omit all interspecific hybrids (BFC10) 
+vcftools --vcf ../../data/processed_vcf/01_popgen_spb_hybrid.vcf --remove ../../data/filtering_data/admixed_individuals.txt --recode --recode-INFO-all --out ../../data/processed_vcf/02_popgen_pure
