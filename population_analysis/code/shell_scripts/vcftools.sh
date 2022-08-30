be7090c4b8e3163495dd4b12ccef55c94ef38561
Joe Gunn <jcg5g9@mail.missouri.edu>
2022-08-30 19:55:15 -0400

    vcftools update

--- population_analysis/code/shell_scripts/vcftools.sh
+++ population_analysis/code/shell_scripts/vcftools.sh
@@ -15,4 +15,7 @@ module load vcftools/vcftools-v0.1.14
 
 # Commands with srun will run on all cores in the allocation
 ## filter 01: omit Spotted Bass
-vcftools --vcf ../../data/processed_vcf/finerad.vcf --remove ../../data/filtering_data/bfc10.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_finerad_spb_hybrid
+#vcftools --vcf ../../data/processed_vcf/finerad.vcf --remove ../../data/filtering_data/bfc10.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_finerad_spb_hybrid
+
+## filter 02: omit Spotted Bass
+vcftools --vcf ../../data/processed_vcf/01_finerad_spb_hybrid.vcf --remove ../../data/filtering_data/spotted_bass.txt --recode --recode-INFO-all --out ../../data/processed_vcf/02_finerad_spb
