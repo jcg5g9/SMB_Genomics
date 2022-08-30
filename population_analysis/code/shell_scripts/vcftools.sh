38af7739b7fa2fd40b1d43736a803c679539ce01
Joseph C Gunn <jcg5g9@lewis4-r7425-htc5-node835.cobalt.lan>
2022-08-30 18:49:18 -0500

    shell scripts

--- population_analysis/code/shell_scripts/vcftools.sh
+++ population_analysis/code/shell_scripts/vcftools.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
 #SBATCH -p Lewis  # use the Lewis partition
-#SBATCH -J SMB_fineRAD  # give the job a custom name
-#SBATCH -o finerad_structure_withfilters-%j.out  # give the job output a custom name
+#SBATCH -J vcftools  # give the job a custom name
+#SBATCH -o filter_spotted.out  # give the job output a custom name
 #SBATCH -t 0-02:00  # two hour time limit
 
 #SBATCH -N 1  # number of nodes
 #SBATCH -n 1  # number of cores (AKA tasks)
 
-# Commands here run only on the first core
-module load fineradstructure/fineradstructure-0.3.2
+# Load VCFtools module
+
+module load rss/rss-2020
+module load vcftools/vcftools-v0.1.14
 
 # Commands with srun will run on all cores in the allocation
-
-## file conversion 01: convert VCF into haplotype chunks for all Smallmouth Bass and Neosho Bass
-RADpainter hapsFromVCF ../../vcfdata/vcf_pass2_badsamples_qual_missing_mac2_het45_admixed.vcf > ../fineradstructure/hap_withfilters_admixed.txt
-# RADpainter paint ../fineradstructure/hap_withfilters_admixed.txt
-# finestructure -x 100000 -y 100000 -z 1000 ../fineradstructure/hap_withfilters_admixed_chunks.out ../fineradstructure/hap_withfilters_admixed_chunks.mcmc.xml
-# finestructure -m T -x 10000 ../fineradstructure/hap_withfilters_admixed_chunks.out ../fineradstructure/hap_withfilters_admixed_chunks.mcmc.xml hap_withfilters_admixed_chunks.mcmcTree.xml
+## filter 01: omit Spotted Bass
+vcftools --vcf ../../data/processed_vcf/finerad.vcf --remove ../../data/filtering_data/bfc10.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_finerad_spb_hybrid
