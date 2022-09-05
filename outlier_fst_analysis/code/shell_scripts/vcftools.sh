035c8fe413ced2df227f117474677356ba44eaa7
Joseph C Gunn <jcg5g9@lewis4-r7425-htc5-node835.cobalt.lan>
2022-09-05 11:52:02 -0500

    vcftools code

--- outlier_fst_analysis/code/shell_scripts/vcftools.sh
+++ outlier_fst_analysis/code/shell_scripts/vcftools.sh
@@ -19,7 +19,31 @@ module load vcftools/vcftools-v0.1.14
 #vcftools --vcf ../../data/processed_vcf/01_popgen_spb_hybrid.vcf --remove ../../data/filtering_data/spotted_bass.txt --recode --recode-INFO-all --out ../../data/processed_vcf/02_popgen_species_complex
 
 ## filter 02: omit Smallmouth Bass samples to get only Neosho samples for the Neosho Bass population level
-vcftools --vcf ../../data/processed_vcf/02_popgen_species_complex.vcf --remove ../../data/filtering_data/smallmouth_bass.txt --recode --recode-INFO-all --out ../../data/processed_vcf/03_popgen_neosho
+#vcftools --vcf ../../data/processed_vcf/02_popgen_species_complex.vcf --remove ../../data/filtering_data/smallmouth_bass.txt --recode --recode-INFO-all --out ../../data/processed_vcf/03_popgen_neosho
 
 ## filter 03: omit Neosho Bass samples to get only Smallmouth Bass samples for the Smallmouth Bass population level
-vcftools --vcf ../../data/processed_vcf/02_popgen_species_complex.vcf --remove ../../data/filtering_data/neosho_bass.txt --recode --recode-INFO-all --out ../../data/processed_vcf/04_popgen_smallmouth
+#vcftools --vcf ../../data/processed_vcf/02_popgen_species_complex.vcf --remove ../../data/filtering_data/neosho_bass.txt --recode --recode-INFO-all --out ../../data/processed_vcf/04_popgen_smallmouth
+
+## filter 04A: Get outlier SNPs for all black bass samples
+#vcftools --vcf ../../data/processed_vcf/01_popgen_spb_hybrid.vcf --positions ../../data/filtering_data/all_outlier_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_popgen_all_outlier_snps
+
+## filter 04B: Get neutral SNPs for all black bass samples
+#vcftools --vcf ../../data/processed_vcf/01_popgen_spb_hybrid.vcf --positions ../../data/filtering_data/all_neutral_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/01_popgen_all_neutral_snps
+
+## filter 05A: Get outlier SNPs for species complex samples
+#vcftools --vcf ../../data/processed_vcf/02_popgen_species_complex.vcf --positions ../../data/filtering_data/species_complex_outlier_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/02_popgen_species_complex_outlier_snps
+
+## filter 05B: Get neutral SNPs for species complex samples
+#vcftools --vcf ../../data/processed_vcf/02_popgen_species_complex.vcf --positions ../../data/filtering_data/species_complex_neutral_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/02_popgen_species_complex_neutral_snps
+
+## filter 06A: Get outlier SNPs for Neosho Bass samples
+#vcftools --vcf ../../data/processed_vcf/03_popgen_neosho.vcf --positions ../../data/filtering_data/neosho_outlier_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/03_popgen_neosho_outlier_snps
+
+## filter 06B: Get neutral SNPs for Neosho Bass samples
+#vcftools --vcf ../../data/processed_vcf/03_popgen_neosho.vcf --positions ../../data/filtering_data/neosho_neutral_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/03_popgen_neosho_neutral_snps
+
+## filter 07A: Get outlier SNPs for Smallmouth Bass samples
+vcftools --vcf ../../data/processed_vcf/04_popgen_smallmouth.vcf --positions ../../data/filtering_data/smallmouth_outlier_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/04_popgen_smallmouth_outlier_snps
+
+## filter 07B: Get neutral SNPs for Smallmouth Bass samples
+vcftools --vcf ../../data/processed_vcf/04_popgen_smallmouth.vcf --positions ../../data/filtering_data/smallmouth_neutral_snps.txt --recode --recode-INFO-all --out ../../data/processed_vcf/04_popgen_smallmouth_neutral_snps
